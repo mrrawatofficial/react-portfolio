06e64bddca69d9981b0a4a3419161ad9e8c7caaf
Rahul Rawat <rahulrawat520@gmail.com>
2021-02-18 13:31:58 +0530

    Update index.js

--- src/Screens/Banner/index.js
+++ src/Screens/Banner/index.js
@@ -1,30 +1,41 @@
-const index = () => {
-    return (
-        <section style={{position:'relative', overflow:'hidden'}} id="banner">
-            <div className="background">
-                <div className="cube"></div>
-                <div className="cube"></div>
-                <div className="cube"></div>
-                <div className="cube"></div>
-                <div className="cube"></div>
-            </div>
+import { Link } from "react-scroll";
 
-            <div className="banner--section">
-                <div className="banner--content">
-                    <h1>We are India Based Web Development Company</h1>
-                    <p>Our team is experienced with advanced latest technologies and we try to use suitable ones in our client projects! We try not to let your project down anyway!</p>
-                    <div className="banner--btn">
-                        <a href="/" className="cus-btn secondary-btn m-1 my-md-0"> 
-                            <span>More About</span>
-                        </a>
-                        <a href="/" className="cus-btn primary-btn m-1 my-md-0"> 
-                            <span>Contact Us</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+var cubes = [1, 2, 3, 4, 5];
+
+const index = () => {
+  return (
+    <section style={{ position: "relative", overflow: "hidden" }} id="banner">
+      <div className="background">
+        {/* <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div> */}
+        {cubes.map((item, i) => (
+          <div key={i} className="cube"></div>
+        ))}
+      </div>
+
+      <div className="banner--section">
+        <div className="banner--content">
+          <h1>We are India Based Web Development Company</h1>
+          <p>
+            Our team is experienced with advanced latest technologies and we try
+            to use suitable ones in our client projects! We try not to let your
+            project down anyway!
+          </p>
+          <div className="banner--btn">
+            <Link to="about" className="cus-btn secondary-btn m-1 my-md-0">
+              <span>More About</span>
+            </Link>
+            <Link to="contact" className="cus-btn primary-btn m-1 my-md-0">
+              <span>Contact Us</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default index;
